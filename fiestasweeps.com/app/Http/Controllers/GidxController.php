1325e4f162cb114483d257faa360a2240634de28
Rahul Saini <rssaini.26@gmail.com>
2025-08-01 05:15:03 +0530

    gidx service

--- fiestasweeps.com/app/Http/Controllers/GidxController.php
+++ fiestasweeps.com/app/Http/Controllers/GidxController.php
@@ -22,7 +22,7 @@ class GidxController extends Controller
             'customer_id' => 'CUST-' . Str::padLeft($user->id, 4, '0'),
             'ip' => $req->ip()
         ]);
-        dd(json_decode($session, true));
+        dd($session);
         /*
         dd($gidx->customerRegistration([
             'merchant_customer_id' => 'CUST-' . Str::padLeft($user->id, 4, '0'),
