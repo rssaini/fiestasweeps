c299dc189b7a83bfd357f6fa5f60070b4233751e
Rahul Saini <rssaini.26@gmail.com>
2025-08-01 02:07:50 +0530

    geolocation service code added

--- fiestasweeps.com/resources/views/pages/dashboard.blade.php
+++ fiestasweeps.com/resources/views/pages/dashboard.blade.php
@@ -9,6 +9,7 @@
     <link rel="icon" type="image/png" href="{{ asset('assets/favicon-96x96.png') }}" sizes="96x96" />
     <link rel="icon" type="image/svg+xml" href="{{ asset('assets/favicon.svg') }}" />
     <link rel="shortcut icon" href="{{ asset('assets/favicon.ico') }}" />
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="dashboard-container">
