307241441bb30e27c51bdf1dd586881b3bd3ba34
Rahul Saini <rssaini.26@gmail.com>
2025-08-02 11:20:53 +0530

    gidx service

--- fiestasweeps.com/app/Models/Notification.php
+++ fiestasweeps.com/app/Models/Notification.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Notification extends Model
 {
     protected $table = 'notifications';
+    public $timestamps = false;
 
     protected $fillable = ['raw'];
 }
