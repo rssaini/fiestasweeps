2fc30480cdfb5bd7443eebe827bb1fc3b705523d
Rahul Saini <rssaini.26@gmail.com>
2025-08-02 17:01:26 +0530

    gidx service

--- fiestasweeps.com/database/migrations/2025_06_09_191754_add_parent_id_to_users_table.php
+++ fiestasweeps.com/database/migrations/2025_06_09_191754_add_parent_id_to_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreign('parent_id')->references('id')->on('users')->onDelete('set null');
             $table->string('lname')->nullable()->after('parent_id');
             $table->string('phone')->nullable()->after('parent_id');
-            $table->string('dob')->nullable()->after('parent_id');
+            $table->date('dob')->nullable()->after('parent_id');
             $table->tinyInteger('game_stats')->default(0)->after('parent_id');
             $table->tinyInteger('marketting_stats')->default(0)->after('parent_id');
             $table->tinyInteger('online_stats')->default(0)->after('parent_id');
