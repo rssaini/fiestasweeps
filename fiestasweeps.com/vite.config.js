4404fd5abce6cb9b045bd3110567a916632906b3
Rahul Saini <rssaini.26@gmail.com>
2025-08-17 05:28:04 +0530

    DirectCashier Implemetation

--- fiestasweeps.com/vite.config.js
+++ fiestasweeps.com/vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    build: {
+            sourcemap: true, // Enable source maps for production build
+        },
     plugins: [
         laravel({
             input: ['resources/js/main.js'],
